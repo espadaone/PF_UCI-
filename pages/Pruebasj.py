1abfa4ecb47a672209c00b84857224103fd516f6
espadaone <espadapablo@gmail.com>
2023-03-22 16:31:47 +0000

    probando

--- pages/Pruebasj.py
+++ pages/Pruebasj.py
@@ -1,20 +1,26 @@
-# Importamos librerias
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import pymysql
+import os
 
+# Obtener credenciales desde las variables de entorno
+db_host = os.environ.get('DB_HOST')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
 
 # Conexion con ddbb
 connection = pymysql.connect(
-    host='servaz.mysql.database.azure.com',
-    user='Azadmin',
-    password='AZcosmospf08',
-    db='pf_uci'
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    db=db_name
 )
 cursor = connection.cursor()
 
+# Resto del código permanece igual
 # Traemos datos desde la base de datos y guardamos las tablas a usar en datasets.
 cursor.execute("select hadm_id, subject_id, admittime, admission_type, diagnosis from admissions")
 admissions = pd.DataFrame(cursor, columns=("hadm_id", "subject_id", "admittime", "admission_type", "diagnosis"))
